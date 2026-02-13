c6332db3bac62c78f8819bb2251c25a3f76b17f4
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-13 23:33:55 +0100

    crear el componentes aside

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { Aside } from "./components/aside/aside";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer],
+  imports: [RouterOutlet, Header, Footer, Aside],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Aside } from '../aside/aside';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Aside],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
