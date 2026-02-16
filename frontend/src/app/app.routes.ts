b806e14d1246c010c8e01523963911c582613e3c
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-16 09:46:28 +0100

    ajustar estilos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,14 +7,14 @@ import { Crear } from './components/categorias/crear/crear';
 import { Productos } from './components/productos/productos';
 import { CrearProducto } from './components/productos/crear-producto/crear-producto';
 import { EditarProducto } from './components/productos/editar-producto/editar-producto';
-import { ProductoPorCategoria } from './components/producto-por-categoria/producto-por-categoria';
+import { ProductoCategoria } from './components/producto-categoria/producto-categoria';
 
 export const routes: Routes = [
     {path:'',pathMatch:'full',redirectTo:'home'},
     {path:'home',component:Home},
     {path:'usuario/registro',component:Registro},
     {path:'categoria',component:Categorias},
-    {path:'categoria/:id',component:ProductoPorCategoria},
+    {path:'categoria/:id',component:ProductoCategoria},
     {path:'categoria/crear',component:Crear},
     {path:'producto',component:Productos},
     {path:'producto/crear',component:CrearProducto},
--- frontend/src/app/components/producto-categoria/producto-categoria.ts
+++ frontend/src/app/components/producto-categoria/producto-categoria.ts
@@ -4,31 +4,35 @@ import { environment } from '../../../environments/environment';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-producto-por-categoria',
+  selector: 'app-producto-categoria',
   imports: [],
-  templateUrl: './producto-por-categoria.html',
-  styleUrl: './producto-por-categoria.css',
+  templateUrl: './producto-categoria.html',
+  styleUrl: './producto-categoria.css',
 })
-export class ProductoPorCategoria {
+export class ProductoCategoria {
   productos: any = [];
   id:string|null='';
 
   constructor(private cd: ChangeDetectorRef,private route: ActivatedRoute) { }
 
   async ngOnInit() {
-    
     //recuperar parámetro de la url
     this.route.paramMap.subscribe(params => {
       this.id = params.get('id');
+      this.listarPPC();
       this.cd.detectChanges();
     });
     
-    this.productos = await listarProductosPorCategoria(this.id);
-    console.log(this.productos);
-    this.cd.detectChanges();
+    this.listarPPC();
+    
   }
 
   getImageUrl(nombre: string): string {
     return `${environment.backendUrl}/uploads/${nombre}`;
+  }
+
+  async listarPPC(){
+    this.productos = await listarProductosPorCategoria(this.id);
+    this.cd.detectChanges();
   }
 }
