db0de588654e60371f8a9e91502e4906d8b89824
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-17 18:42:58 +0100

    implementar funcionalidad botones incrementar y decrementar en carrito

--- frontend/src/app/components/carrito/carrito.ts
+++ frontend/src/app/components/carrito/carrito.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { RouterLink } from '@angular/router';
 import { statsCarrito } from '../../shared/utils/funciones';
@@ -13,6 +13,8 @@ export class Carrito {
   carrito:any=[];
   stats:any={};
 
+  constructor(private cd: ChangeDetectorRef){}
+
   ngOnInit(){
     const carritoString=localStorage.getItem('carritoTiendaOnline');
     if(carritoString) this.carrito=JSON.parse(carritoString);
@@ -23,4 +25,46 @@ export class Carrito {
   getImageUrl(nombre: string): string {
       return `${environment.backendUrl}/uploads/${nombre}`;
     }
+  
+  vaciarCarrito(){
+    localStorage.removeItem('carritoTiendaOnline');
+    location.reload();
+  }
+
+  eliminarProducto(id:number){
+    console.log(this.carrito);
+    this.carrito=this.carrito.filter((elem:any)=>elem.id !== id);
+    localStorage.setItem('carritoTiendaOnline',JSON.stringify(this.carrito));
+    this.cd.detectChanges();
+  }
+
+  decrementar(id:number){
+    
+    const cantidad=this.getCantidadFromProd();
+    if(cantidad>0){
+      this.carrito=this.carrito.map((elem:any)=>{
+        if(elem.id===id){
+          elem.cantidad=parseInt(elem.cantidad)-1;
+          return elem;
+        }
+        return elem;
+      });
+      localStorage.setItem('carritoTiendaOnline',JSON.stringify(this.carrito));
+    }
+  }
+
+  incrementar(id:number){
+    this.carrito=this.carrito.map((elem:any)=>{
+        if(elem.id===id){
+          elem.cantidad=parseInt(elem.cantidad)+1;
+          return elem;
+        }
+        return elem;
+      });
+    localStorage.setItem('carritoTiendaOnline',JSON.stringify(this.carrito));
+  }
+
+  getCantidadFromProd(){
+    return 3;
+  }
 }
